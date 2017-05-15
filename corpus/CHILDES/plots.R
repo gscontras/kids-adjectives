cc406706da9506558def09383f3b4688fed03882
Greg Scontras <greg@dhcp-v248-043.sa.reshsg.uci.edu>
2017-05-14 20:18:14 -0700

    BUCLD abstract

--- corpus/CHILDES/plots.R
+++ corpus/CHILDES/plots.R
@@ -38,95 +38,31 @@ head(d)
 
 d <- d[d$numadj=="2",]
 
-d$age = factor(d$age,labels = c("2","2-4","3","4","adult"))
-d$age = factor(d$age,levels = c("2-4","2","3","4","adult"))
+d <- d[d$age!="24",]
+
+#d$age = factor(d$age,labels = c("2","3","4","adult"))
+#d$age = factor(d$age,levels = c("2","3","4","adult"))
 d$label = factor(d$label,labels = c("adult","child-directed","child-produced"))
 d$class = factor(d$class,labels = c("age","color","dimension","dimensiomn\nage\nphysical","material","physical","shape","shape\ncolor\nmaterial","value"))
 d$class = factor(d$class,levels = c("dimension","value","age","physical","shape","color","material","dimensiomn\nage\nphysical","shape\ncolor\nmaterial"))
 
-ggplot(data=d[d$binned=="no"&d$age!="2-4",],aes(x=reorder(class,-distance,mean),y=distance,fill=age))+
-  geom_bar(stat="identity",position=position_dodge(.9),color="black")+
-  geom_errorbar(aes(ymin=CI1, ymax=CI2, x=reorder(class,distance,is.ordered=T), width=0.1),position=position_dodge(.9))+
-  xlab("\nsemantic class")+
-  #labs(fill="experiment") +
-  facet_grid(.~label, scales = "free_x")+
-  ylab("distance\n")+
-  ylim(0,1)+
-  theme_bw()+
-  theme(axis.text.x = element_text(angle = 45, hjust = 1,vjust=1))+
-  scale_fill_manual(values=c("#F8766D","#A3A500","#00BF7D","#00B0F6","#E76BF3"))
-#ggsave("~/git/kids-adjectives/corpus/CHILDES/plots/individual-age-plot.pdf",width=8.5,height=3)
-  
 
-ggplot(data=d[d$binned=="yes"&d$age!="2-4",],aes(x=reorder(class,-distance,mean),y=distance,fill=age))+
-  geom_bar(stat="identity",position=position_dodge(.9),color="black")+
-  geom_errorbar(aes(ymin=CI1, ymax=CI2, x=reorder(class,distance,is.ordered=T), width=0.1),position=position_dodge(.9))+
-  xlab("\nsubjectivity level")+
-  #labs(fill="experiment") +
-  facet_grid(.~label, scales = "free_x")+
-  ylab("distance\n")+
-  ylim(0,1)+
-  theme_bw()+
-  theme(axis.text.x = element_text(angle = 45, hjust = 1,vjust=1))+
-  scale_fill_manual(values=c("#F8766D","#A3A500","#00BF7D","#00B0F6","#E76BF3"))
-#ggsave("~/git/kids-adjectives/corpus/CHILDES/plots/individual-age-plot-binned.pdf",width=8.5,height=3.6)
-  
-ggplot(data=d[d$binned=="no"&(d$age=="2-4"|d$age=="adult"),],aes(x=reorder(class,-distance,mean),y=distance))+
-  geom_bar(stat="identity",position=position_dodge(.9),color="black",fill="#E76BF3")+
-  geom_errorbar(aes(ymin=CI1, ymax=CI2, x=reorder(class,distance,is.ordered=T), width=0.1),position=position_dodge(.9))+
-  xlab("\nsemantic class")+
-  #labs(fill="experiment") +
-  facet_grid(.~label )+
-  ylab("distance\n")+
-  #ylim(0,1)+
-  theme_bw()+
-  theme(axis.text.x = element_text(angle = 45, hjust = 1,vjust=1))
-  #ggsave("~/git/kids-adjectives/corpus/CHILDES/plots/2-4-plot.pdf",width=8.5,height=3)
-  
-  
-ggplot(data=d[d$binned=="yes"&(d$age=="2-4"|d$age=="adult"),],aes(x=reorder(class,-distance,mean),y=distance,fill=age))+
-  geom_bar(stat="identity",position=position_dodge(.9),color="black",fill="#E76BF3")+
-  geom_errorbar(aes(ymin=CI1, ymax=CI2, x=reorder(class,distance,is.ordered=T), width=0.1),position=position_dodge(.9))+
-  xlab("\nsubjectivity level")+
-  #labs(fill="experiment") +
-  facet_grid(.~label )+
-  ylab("distance\n")+
-  #ylim(0,1)+
-  theme_bw()+
-  theme(axis.text.x = element_text(angle = 45, hjust = 1,vjust=1))
-#ggsave("~/git/kids-adjectives/corpus/CHILDES/plots/2-4-plot-binned",width=3.5,height=8)
-
-
-## two by four plots
+## semantic class plots
 ggplot(data=d[d$binned=="no"&d$label=="adult",],aes(x=class,y=distance,fill=age))+
   geom_bar(stat="identity",position=position_dodge(.9),color="black")+
   geom_errorbar(aes(ymin=CI1, ymax=CI2, x=class, width=0.1),position=position_dodge(.9))+
-  xlab("semantic class")+
+  xlab("")+
   #labs(fill="experiment") +
   facet_grid(.~label, scales = "free_x")+
-  ylab("distance\n")+
+  ylab("")+
   ylim(0,1)+
   theme_bw()+
   theme(axis.text.x = element_text(angle = 90, hjust = 1,vjust=.5))+
   guides(fill=FALSE)+
   scale_fill_manual(values=c("#E76BF3"))
 #ggsave("~/git/kids-adjectives/corpus/CHILDES/plots/adult-plot.pdf",width=3,height=4.5)
-#ggsave("~/git/kids-adjectives/corpus/CHILDES/plots/adult-plot-short.pdf",width=2.35,height=1.95)
+#ggsave("~/git/kids-adjectives/corpus/CHILDES/plots/adult-plot-short.pdf",width=2.2,height=2.1)
 
-ggplot(data=d[d$binned=="yes"&d$label=="adult",],aes(x=class,y=distance,fill=age))+
-  geom_bar(stat="identity",position=position_dodge(.9),color="black")+
-  geom_errorbar(aes(ymin=CI1, ymax=CI2, x=class, width=0.1),position=position_dodge(.9))+
-  xlab("subjectivity level")+
-  #labs(fill="experiment") +
-  facet_grid(.~label, scales = "free_x")+
-  ylab("distance\n")+
-  ylim(0,1)+
-  theme_bw()+
-  theme(axis.text.x = element_text(angle = 90, hjust = 1,vjust=.5))+
-  guides(fill=FALSE)+
-  scale_fill_manual(values=c("#E76BF3"))
-#ggsave("~/git/kids-adjectives/corpus/CHILDES/plots/adult-plot-binned.pdf",width=3,height=4.5)
-#ggsave("~/git/kids-adjectives/corpus/CHILDES/plots/adult-plot-binned-short.pdf",width=2.35,height=2.1)
 
 ggplot(data=d[d$binned=="no"&d$label!="adult",],aes(x=class,y=distance,fill=age))+
   geom_bar(stat="identity",position=position_dodge(.9),color="black")+
@@ -134,27 +70,47 @@ ggplot(data=d[d$binned=="no"&d$label!="adult",],aes(x=class,y=distance,fill=age)
   xlab("semantic class")+
   #labs(fill="experiment") +
   facet_grid(age~label, scales = "free_x")+
-  ylab("")+
+  ylab("distance\n")+
   ylim(0,1)+
   theme_bw()+
   theme(axis.text.x = element_text(angle = 90, hjust = 1,vjust=.5))+
   guides(fill=FALSE)+
   scale_fill_manual(values=c("#F8766D","#A3A500","#00BF7D","#00B0F6","#E76BF3"))
-#ggsave("~/git/kids-adjectives/corpus/CHILDES/plots/individual-age-plot-vertical.pdf",width=4.1,height=4.5)
+#ggsave("~/git/kids-adjectives/corpus/CHILDES/plots/individual-age-plot-vertical.pdf",width=4.2,height=4)
+
+## subjectivity level plots
+
+d$class = factor(d$class,levels = c("dimension","value","age","physical","shape","color","material","dimensiomn\nage\nphysical","shape\ncolor\nmaterial"))
+d$class = factor(d$class,labels = c("dimension","high","age","physical","shape","color","material","middle","low"))
+
+ggplot(data=d[d$binned=="yes"&d$label=="adult",],aes(x=class,y=distance,fill=age))+
+  geom_bar(stat="identity",position=position_dodge(.9),color="black")+
+  geom_errorbar(aes(ymin=CI1, ymax=CI2, x=class, width=0.1),position=position_dodge(.9))+
+  xlab("")+
+  #labs(fill="experiment") +
+  facet_grid(.~label, scales = "free_x")+
+  ylab("")+
+  ylim(0,1)+
+  theme_bw()+
+  #theme(axis.text.x = element_text(angle = 90, hjust = 1,vjust=.5))+
+  guides(fill=FALSE)+
+  scale_fill_manual(values=c("#E76BF3"))
+#ggsave("~/git/kids-adjectives/corpus/CHILDES/plots/adult-plot-binned.pdf",width=3,height=4.5)
+#ggsave("~/git/kids-adjectives/corpus/CHILDES/plots/adult-plot-binned-short.pdf",width=2.2,height=1.6)
 
 ggplot(data=d[d$binned=="yes"&d$label!="adult",],aes(x=class,y=distance,fill=age))+
   geom_bar(stat="identity",position=position_dodge(.9),color="black")+
   geom_errorbar(aes(ymin=CI1, ymax=CI2, x=class, width=0.1),position=position_dodge(.9))+
-  xlab("subjectivity level")+
+  xlab("\n\nsubjectivity level")+
   #labs(fill="experiment") +
   facet_grid(age~label, scales = "free_x")+
   ylab("")+
   ylim(0,1)+
   theme_bw()+
-  theme(axis.text.x = element_text(angle = 90, hjust = 1,vjust=.5))+
+  #theme(axis.text.x = element_text(angle = 90, hjust = 1,vjust=.5))+
   guides(fill=FALSE)+
   scale_fill_manual(values=c("#F8766D","#A3A500","#00BF7D","#00B0F6","#E76BF3"))
-#ggsave("~/git/kids-adjectives/corpus/CHILDES/plots/individual-age-plot-binned-vertical.pdf",width=4.1,height=4.5)
+#ggsave("~/git/kids-adjectives/corpus/CHILDES/plots/individual-age-plot-binned-vertical.pdf",width=4.1,height=3.9)
 
 
 
